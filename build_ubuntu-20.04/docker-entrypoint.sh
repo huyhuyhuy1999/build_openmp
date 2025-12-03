311235b1e41ff48b55e633c27ec7d17c051dc363
Aker <qjayjayq@gmail.com>
2025-12-04 05:14:03 +0700

    abc

---
+++ build_ubuntu-20.04/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+[ -z $CONFIG ] && config=Release || config="$CONFIG"
+[ -z $TARGET_BUILD_ARCH ] && target_build_arch=x86 || target_build_arch="$TARGET_BUILD_ARCH"
+
+cd build \
+&&
+cmake .. \
+    -G Ninja \
+    -DTARGET_BUILD_ARCH=$target_build_arch \
+    -DCMAKE_BUILD_TYPE=$config \
+&&
+cmake \
+    --build . \
+    --config $config \
+    --parallel $(nproc)
